eee832843341294b2f7ac170e3c0727f271d6cdd
rootreboot <JoaoMdCouto@users.noreply.github.com>
2020-01-16 22:28:20 +0000

    post, put, delete artists

--- api/artists.js
+++ api/artists.js
@@ -36,4 +36,103 @@ artistsRouter.get("/:artistId", (req, res, next) => {
   res.status(200).json({ artist: req.artist });
 });
 
+const validateArtistRequest = (req, res, next) => {
+  const artist = req.body.artist;
+  artist.is_currently_employed = artist.is_currently_employed === 0 ? 0 : 1;
+
+  if (!artist.name || !artist.dateOfBirth || !artist.biography) {
+    return res.sendStatus(400);
+  } else {
+    next();
+  }
+};
+
+artistsRouter.post("/", validateArtistRequest, (req, res, next) => {
+  const sql =
+    "INSERT INTO Artist (name, date_of_birth, biography, is_currently_employed) " +
+    " VALUES ($name, $dateOfBirth, $biography, $isCurrentlyEmployed)";
+
+  const value = {
+    $name: req.body.artist.name,
+    $dateOfBirth: req.body.artist.dateOfBirth,
+    $biography: req.body.artist.biography,
+    $isCurrentlyEmployed: req.body.artist.is_currently_employed
+  };
+
+  db.run(sql, value, function(error) {
+    if (error) {
+      next(error);
+    } else {
+      const sql = "SELECT * FROM Artist WHERE id = $artistId";
+      const value = { $artistId: this.lastID };
+      db.get(sql, value, (err, artist) => {
+        if (err) {
+          next(err);
+        } else {
+          res.status(201).json({ artist: artist });
+        }
+      });
+    }
+  });
+});
+
+artistsRouter.put("/:artistId", validateArtistRequest, (req, res, next) => {
+  const newArtist = req.body.artist;
+  const artistId = Number(req.params.artistId);
+  if (!newArtist.id || newArtist.id !== artistId) {
+    newArtist.id = artistId;
+  }
+
+  const sql =
+    "UPDATE Artist SET " +
+    "name = $name, " +
+    "date_of_birth = $dateOfBirth, " +
+    "biography = $biography, " +
+    "is_currently_employed = $isCurrentlyEmployed " +
+    "WHERE Artist.id = $artistId";
+
+  const values = {
+    $name: req.body.artist.name,
+    $dateOfBirth: newArtist.dateOfBirth,
+    $biography: newArtist.biography,
+    $isCurrentlyEmployed: newArtist.is_currently_employed,
+    $artistId: newArtist.id
+  };
+
+  db.run(sql, values, error => {
+    if (error) {
+      next(error);
+    } else {
+      const sql = "SELECT * FROM Artist WHERE id = $artistId";
+      const value = { $artistId: newArtist.id };
+      db.get(sql, value, (err, artist) => {
+        if (err) {
+          next(err);
+        } else {
+          res.status(200).json({ artist: artist });
+        }
+      });
+    }
+  });
+});
+
+artistsRouter.delete("/:artistId", (req, res, next) => {
+  const sql =
+    "UPDATE Artist SET is_currently_employed = 0 WHERE id = $artistId";
+  const values = { $artistId: req.params.artistId };
+
+  db.run(sql, values, err => {
+    if (err) {
+      next(err);
+    } else {
+      db.get(
+        `SELECT * FROM Artist WHERE id = $artistId = ${req.params.artistId}`,
+        (err, artist) => {
+          res.status(200).json({ artist: artist });
+        }
+      );
+    }
+  });
+});
+
 module.exports = artistsRouter;
